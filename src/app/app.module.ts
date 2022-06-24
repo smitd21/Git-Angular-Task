6488d7e395cbe16c7f5776c0f9e30b3c18eeaeb9
Smit Desai <smitdesai@MADMINs-MacBook-Pro-4.local>
2022-06-24 13:05:20 +0530

    Added nav-bar ✅

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './MyComponents/employee-list/employee-list.component';
 import { EmployeeDetailsComponent } from './MyComponents/employee-details/employee-details.component';
+import { EmployeeService } from './employee.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    EmployeeListComponent,
-    EmployeeDetailsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, EmployeeListComponent, EmployeeDetailsComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
