83ed34ad7799d9793c8288128a2e257fb967f010
Smit Desai <smitdesai@MADMINs-MacBook-Pro-4.local>
2022-06-24 14:05:53 +0530

    Added routes and linked employee-list component ✅

--- src/app/MyComponents/employee-list/employee-list.component.ts
+++ src/app/MyComponents/employee-list/employee-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../employee.service';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
+  styleUrls: ['./employee-list.component.css'],
 })
 export class EmployeeListComponent implements OnInit {
-
-  constructor() { }
+  public employees = [];
+  constructor(private _employeeService: EmployeeService) {}
 
   ngOnInit(): void {
+    this.employees = this._employeeService.getEmployee();
   }
-
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './MyComponents/employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './MyComponents/employee-details/employee-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employee-list', component: EmployeeListComponent },
+  { path: 'employee-details', component: EmployeeDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
