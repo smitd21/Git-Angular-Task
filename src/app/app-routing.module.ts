e4f3bf2b042eac99c557277da36acf0723e211db
Smit Desai <smitdesai@MADMINs-MacBook-Pro-4.local>
2022-06-24 14:51:59 +0530

    Added Home Component to home route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './MyComponents/home/home.component';
 import { EmployeeListComponent } from './MyComponents/employee-list/employee-list.component';
 import { EmployeeDetailsComponent } from './MyComponents/employee-details/employee-details.component';
 
 const routes: Routes = [
+  // { path: '', redirectTo: '/employee-list', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'employee-list', component: EmployeeListComponent },
   { path: 'employee-details', component: EmployeeDetailsComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './MyComponents/employee-list/employee-list.component';
 import { EmployeeDetailsComponent } from './MyComponents/employee-details/employee-details.component';
 import { EmployeeService } from './employee.service';
+import { HomeComponent } from './MyComponents/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, EmployeeListComponent, EmployeeDetailsComponent],
+  declarations: [AppComponent, EmployeeListComponent, EmployeeDetailsComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [EmployeeService],
   bootstrap: [AppComponent],
